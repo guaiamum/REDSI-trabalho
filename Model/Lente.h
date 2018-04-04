4da8c2bbade44ee601ab23d23a79b1326e859da4
Cainã Brazil <cainabrazil@gmail.com>
2018-04-04 11:34:00 +0100

    ajuste minimo

--- Model/Lente.h
+++ Model/Lente.h
@@ -7,7 +7,7 @@ class Lente : public Produto{
   bool Zoom;
   int Zoom_min;
   int Zoom_max;
-  float Abertura;  //1.0, 1.1, 1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.5, 2.8, 3.2, 3.5, 4, 4.5, 5.0, 5.6, 6.3, 7.1, 8, 9, 10, 11, 13, 14, 16, 18, 20, 22
+  float Abertura; // 1.0, 1.1, 1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.5, 2.8, 3.2, 3.5, 4, 4.5, 5.0, 5.6, 6.3, 7.1, 8, 9, 10, 11, 13, 14, 16, 18, 20, 22
 public:
   Lente();
   Lente(string marca, string modelo, int peso, bool zoom, int min, int max, float abertura);
